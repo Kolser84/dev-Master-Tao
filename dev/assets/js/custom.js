148df35b4f8c591a562169ffcfa96ad50e3fa170
Kolser84 <kurssakovo@gmail.com>
2025-07-02 01:01:11 +0300

    styles added styles to banner slider

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,18 +1,16 @@
 const btnBurger = document.querySelector('.hamburger')
 btnBurger.addEventListener('click', () => btnBurger.classList.toggle('is-active')) 
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
+const swiperBanner = new Swiper('.swiper-banner', {
    
-    loop: true,
+  loop: true,
+  spaceBetween: 24,
   
-    // If we need pagination
   pagination: {
       clickable: true,
       el: '.swiper-pagination',
     },
   
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
