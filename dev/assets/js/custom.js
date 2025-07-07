2f567e4188cb5bc6d4b4c83cc496c290efd6b9d5
Kolser84 <kurssakovo@gmail.com>
2025-07-08 00:35:07 +0300

    feat: adoptation slided for mobile

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,22 +1,36 @@
 const btnBurger = document.querySelector('.hamburger')
 btnBurger.addEventListener('click', () => btnBurger.classList.toggle('is-active')) 
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper-banner', {
   // Optional parameters
-
   loop: true,
-
-  // If we need pagination
-  pagination: {
-    clickable: true,
-    el: '.swiper-pagination',
-  },
+  spaceBetween: 24,
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
+
+  // If we need pagination
+
+  pagination: {
+    clickable: true,
+    el: '.swiper-pagination',
+  },
+
+
+  
+
+   // Responsive breakpoints
+   breakpoints: {
+    992: {
+        
+    }
+  }
+
+
 
  
 });
